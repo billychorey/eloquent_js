338d00a3ef8d62a22483e4124c7d42fc03282bfc
billychorey <billychorey@gmail.com>
2015-09-14 23:55:02 -0400

    none of this makes sense. As in zero... nothing makes sense

---
+++ eloquent_js/script.js
@@ -0,0 +1,154 @@
+/* js file */
+
+(function( window, undefined ){
+
+})( window, undefined );
+
+//1.1
+//fractional numbers are written by using a "dot
+// 3.3333
+
+//1.2
+//large numbers are written with e for exponent
+//2.3543e4
+
+////arithmetic
+//24+6*3
+//arithmetic follows pemdas
+
+//1.4
+//"strings use quotation marks"
+
+//1.5
+//to make special characters appear, use the backslash
+// to make a new line, "here is \n a new line"
+//skipped other examples of strings
+//typeof operator produces a string naming the value you give it
+//console.log(typeof 5) // returns a numbers
+//console.log9typeof "" returns a string
+//boolean true/false yes/no
+
+//console.log(5>4) returns true
+//!= not equal to, provide two values and will return a boolean t/function() {};
+//logic && || and or 
+// && both must be true
+
+
+// ||or, one must be true... or both false returns false
+// || value returns value to left if can be considered true
+// || value returns right value if not
+// I don't know what this is saying
+
+
+//any fragment of code that produces a value is an expression
+//that value can then be used by enclosing the expression
+// a statement is a full sentence vs a fragment
+//a program is a list of statements
+
+//variables hold values
+//variables are defined by keyword var
+//after defined, it can be used as an expression
+
+
+//var 5k = 15;
+//console.log(5k/5); should return 3
+
+//variable values can be changed at any time
+//var hungry = "yes"
+//console.log(hungry) returns yes
+//hungry = "maybe"
+//console.log(hungry) returns maybe
+
+//can use and change as needed
+// var myTirednessFromOneToTen = "8"
+// myTirednessFromOneToTen = myTirednessFromOneToTen = 7
+//console.log(myTirednessFromOneToTen)
+//should return 15
+
+//Math.max takes values and returns the greatest
+//console.log(Math.max (5,4,65,345,2342)) probably goiung to return 2342
+//Math.min is the opposite. 
+//console.log (Math.min(1,54)+5432 probably going to choose the 1
+
+//confirm can be boolean, ok is true cancel false
+//prompt asks open question where user types a response
+//conditional execution
+// if and only if one condition... else run other response
+// if skips a statement depending if boolean is true or false
+
+//print all even numbers 0-12 
+//console.log(0) etc
+
+//loop allows repeat
+//var number = 0
+//while (number <=48) {
+/*	console.log(number);
+	number = number = 1;
+}
+//should return 0, 1, 2, 3, 4, etc tp 48
+
+
+
+//do loop executes its body at least once and re evaluates after
+the for loop is more comprehensive...
+for a certain variable, while the value meets this parameter
+do this equation and return the number
+
+loops are broken when produce false or if there is a break
+eg find first number greater than 6 and divisible by 3
+for (var current = 6; ;current ++){if (current %3 ===0)}
+***************************************
+CHAPTER 2 REVIEW QUESTIONS
+#(+=#) is the hint
+
+var # = 0;
+while (# <= 7) {
+  console.log(#;
+  # = # + 1;
+}
+***************************************
+
+
+fizzbuzz console.log all numbers 1-100
+for divisible by 3 print fizz for 5 print buzz
+for 3 and 5 fizz buzz
+
+the hint says else if else chain
+if %3 === 0 then fizz
+if %5 === 0 then buzz 
+
+for (var x=1) while x<100, x++ keep adding one plus one etc 
+then as it hits divisible by 3 and 5 console.log fizz/buzz
+
+else if 
+else if
+else
+
+*********I have no idea *************
+ 
+
+
+
+
+ chess board... this is worse
+
+
+ ************************************
+
+ ch3
+ function is variable definition where value given is a function
+ eg variable of tired equals the product of a million times a million
+ var tired = function(x) {
+	return 1000000 * 1000000;
+ }
+ there are parameters and a body which contains what statements are to be executed.
+
+var sound = function () {
+	console.log("train whistle")
+}
+sound(); returns train whistle
+
+
+
+
+
